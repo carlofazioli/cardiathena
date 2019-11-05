f3aebac903c0b6b0a1f3c06029bbab29de7c464b
akruza <akruza@groupw.com>
2019-11-05 17:08:17 -0500

    removed state map, updated tests, formatted for PEP8

--- adjudicator/state.py
+++ adjudicator/state.py
@@ -1,70 +1,98 @@
 import numpy as np
-"""Basic Implementation of the Card Game State"""
+from itertools import product
+from random import shuffle
+
+# definitions for the card values and suit values in a 52 card deck
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['C', 'D', 'S', 'H']
+
+# constants
+player_hand = [1, 2, 3, 4]
+player_won = [11, 12, 13, 14]
+player_current = [21, 22, 23, 24]
+unknown = 0
 
 
 class State(object):
 
-    """State DOCSTRING"""
+    """
+
+    """
 
     def __init__(self):
-        self.card_vector = np.zeros((1, 52), dtype=int)
-        self.Lookup_table = self.build_lookup_table()
+        """
+        inits the state and randomly assigns player
+
+        """
+        self.card_vector = None
+        self.shuffle()
 
     def __repr__(self):
-        """ How we want to represent the class"""
-        ret = ""
-        for row in self.card_vector:
-            for column in row:
-                ret+=" "+str(column)
-        return ret
+        """
+        the human readable representation of the state vector
 
-    """ Change the Encoding of one of the card"""
-    def setEncoding(self,encoding , card):
-        index = int(self.Lookup_table[card])
-        print(index)
-        print(self.card_vector.shape)
-        self.card_vector[0,index]=encoding
+        :return: string
+        """
+        out = '|'.join([f'{x:3}' for x in self.card_position.keys()]) + "\n" + '|'.join([f'{x:3}' for x in self.card_vector])
 
-        """Function for the agent . Enter a player from 0-3 and it will blank
-        out everything that does not belong to that player"""
-    def Hide_encoding(self,player):
-        player_1=[1,11,21]
-        player_2=[2,12,22]
-        player_3=[3,13,23]
-        player_4=[4,14,24]
-        players = [player_1,player_2,player_3,player_4]
+        return out
+
+    def shuffle(self):
+        players = [1] * 13 + [2] * 13 + [3] * 13 + [4] * 13
+        shuffle(players)
+        self.card_vector = np.array(players)
+
+    def set_encoding(self, encoding, card):
+        """
+        encode a value to the state vector
+
+        :param encoding: value to be encoded on the state vector
+        :param card: position in the state vector the be encoded
+        :return: None
+        """
+
+        element = self.card_position[card]
+        self.card_vector[element] = encoding
+
+    def get_encoding(self, card):
+        """
+
+        :param card: position in the state vector to get value
+        :return: encoded value
+        """
+        element = self.card_position[card]
+
+        return self.card_vector[element]
+
+    def hide_encoding(self, player):
+        player_1 = [1, 11, 21]
+        player_2 = [2, 12, 22]
+        player_3 = [3, 13, 23]
+        player_4 = [4, 14, 24]
+        players = [player_1, player_2, player_3, player_4]
         ret = self.card_vector
-        nrow=0
-        ncolumn=0
+        nrow = 0
+        ncolumn = 0
         for row in ret:
 
             for column in row:
                 if column in players[player]:
                     continue
                 else:
-                    ret[nrow,ncolumn]=0
-                ncolumn+=1
-            nrow==1
+                    ret[nrow , ncolumn] = 0
+                ncolumn += 1
+            nrow == 1
 
         return ret
 
-    """For now the table data is being read from a taxt file but i 
-    think it may be a good idea to hard code this section"""
-    def build_lookup_table(self):
-        file_db = open("Card_State_Map.txt" , "r+")
-        ret = {}
-        for line in file_db:
-            split = line.split(",")
-            ret[split[0]]=split[1]
-        return ret
+    @property
+    def card_position(self):
+        """
+        builds a python dictionary that lists the element position for each card on the state vector
 
-"""Testing"""
+        :return:
+        """
 
-Game_State_d = Game_State()
-print(Game_State_d)
-Game_State_d.setEncoding(11,"2C")
-print(Game_State_d)
-Game_State_d.setEncoding(20,"AD")
-print(Game_State_d)
+        return {f'{v[1]}{v[0]}': i for i, v in enumerate(product(suits, cards))}
 
 
--- adjudicator/tests/test_state.py
+++ adjudicator/tests/test_state.py
@@ -1,8 +1,18 @@
 import unittest
 from ..state import State
+import numpy as np
 
 
 class TestState(unittest.TestCase):
 
     def setUp(self):
         self.state = State()
+
+    def test_shuffle(self):
+        self.assertIsNotNone(self.state.card_vector)
+        self.assertFalse(np.all(self.state.card_vector == np.array([1] * 13 + [2] * 13 + [3] * 13 + [4] * 13)))
+
+    def test_set_and_get_encoding(self):
+        self.state.set_encoding(11, 'AH')
+        self.assertEqual(self.state.card_vector[-1], 11)
+        self.assertEqual(self.state.get_encoding('AH'), 11)
