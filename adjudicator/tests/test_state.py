95ec89d8c41e4c4abf455b9ddce5cf9eb4662bd5
David <34842254+davidjha@users.noreply.github.com>
2019-11-09 01:58:14 +0000

    Added test case for hide_encoding

--- adjudicator/tests/test_state.py
+++ adjudicator/tests/test_state.py
@@ -16,3 +16,21 @@ class TestHeartsState(unittest.TestCase):
         self.state.set_encoding(11, 'AH')
         self.assertEqual(self.state.values[-1], 11)
         self.assertEqual(self.state.get_encoding('AH'), 11)
+        
+    def test_hide_encoding(self):
+        self.state.set_encoding(12, '5S')
+        self.state.set_encoding(13, 'JD')
+        self.state.set_encoding(21, '6H')
+        expected_p1_state = [i for i in self.state.values]
+        self.assertTrue(np.all(self.state.values == np.array(expected_p1_state)))
+        for x in range(52):
+            if expected_p1_state[x] != 1:
+                expected_p1_state[x] = 0
+
+        expected_p1_state[43] = 21
+        p1_state = self.state.hide_encoding(0)
+        self.assertTrue(np.all(p1_state == np.array(expected_p1_state)))
+
+
+if __name__ == "__main__":
+    unittest.main()
