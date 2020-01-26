20bf5421dc1967432b79edc78cb0930efbfa6f49
davidjha <davidjha21@gmail.com>
2020-01-26 16:26:01 -0500

    updated sqlalchemy_example.py, adds a table to the database.

--- database/sqlalchemy_example.py
+++ database/sqlalchemy_example.py
@@ -1,15 +1,23 @@
-from sqlalchemy import create_engine, Column, Integer, select
+from sqlalchemy import create_engine, Column, Integer, String, Text, Table
+from sqlalchemy.dialects.mysql import JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'mysql+pymysql://remote_usr:Mn1y7eId@localhost/state_db'
+
+DB_USER = 'remote_usr'
+DB_PASS = 'GH9BgFaF'
+DB_HOST = 'localhost'
+DB_PORT = '3306'
+DATABASE = 'state_db'
+
+DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DATABASE)
 
 
 def create_database(database_name):
     conn = engine.connect()
     try:
-        conn.execute("CREATE DATABASE " + database_name)
+        conn.execute("CREATE DATABASE IF NOT EXISTS" + database_name)
     except SQLAlchemyError as err:
         print(err)
 
@@ -34,8 +42,20 @@ def drop_table(table_name):
     conn.close()
 
 
-def show_db():
-    print(engine.table_names())
+def add_data(uuid, st):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stateactionhistory = StateActionHistory(gameuuid=uuid, state=st)
+    session.add(stateactionhistory)
+    session.commit()
+
+
+def get_data(uuid):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(StateActionHistory).all()
+    for row in query:
+        print("gameuuid: ", row.gameuuid, "state: ", row.state, "action: ", row.action)
 
 
 # Interface to the database configured for database dialect and dbapi
@@ -48,20 +68,14 @@ class StateActionHistory(Base):
     # Table name
     __tablename__ = 'stateactionhistory'
     # Create our table columns
-    gameuuid = Column('gameuuid', Integer, primary_key=True)
-    state = Column('state', Integer)
-    action = Column('action', Integer)
-
-    def __init_(self, gameuuid, state, action):
-        self.gameuuid = gameuuid
-        self.state = state
-        self.action = action
+    gameuuid = Column('gameuuid', Integer, primary_key=True, nullable=False)
+    state = Column('state', JSON)
 
 
-# create_database("state_db")
-# drop_database("state_db")
-# drop_table('statehistory')
+
 
 Base.metadata.create_all(bind=engine)
 
-show_db()
+# create_database("state_db")
+# drop_database("state_db")
+drop_table('stateactionhistory')
