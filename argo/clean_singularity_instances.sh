2625d12612772301b2bdc361924e6532d9a8442b
David <34842254+davidjha@users.noreply.github.com>
2020-01-30 21:59:14 -0500

    Moved redundant commands remove argo log files

--- argo/clean_singularity_instances.sh
+++ argo/clean_singularity_instances.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm /scratch/$USER/logs_argo/err/*.err 
-rm /scratch/$USER/logs_argo/out/*.out
 rm /scratch/$USER/mysql/run/mysqld/*.lock
 rm /scratch/$USER/mysql/var/lib/mysql/*.lock
-rm /scratch/$USER/runtimes
-rm /scratch/$USER/singularity/instances/sing/*.edu
+rm -r /scratch/$USER/singularity/instances/sing/*.edu
