3813f719a4957d4b280640abaa09f8411a1fbe0e
davidjha <davidjha21@gmail.com>
2020-01-30 19:12:46 -0500

    Added clean logs and clean singularity scripts

---
+++ argo/clean_singularity_instances.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm /scratch/$USER/logs_argo/err/*.err 
+rm /scratch/$USER/logs_argo/out/*.out
+rm /scratch/$USER/mysql/run/mysqld/*.lock
+rm /scratch/$USER/mysql/var/lib/mysql/*.lock
+rm /scratch/$USER/runtimes
+rm /scratch/$USER/singularity/instances/sing/*.edu
