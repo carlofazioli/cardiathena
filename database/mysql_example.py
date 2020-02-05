d0b46b9ceee52d91044fcfc9803fa9397703f361
davidjha <davidjha21@gmail.com>
2020-02-05 10:46:14 -0500

    Modified insertions to accept one parameter for state data. State data is converted to json from a list of dictionaries: State_data = {    'state': list of states    'actions': list of actions    'scores': list of scores } This may simply retrieval and conversion back to the agents.

--- database/mysql_example.py
+++ database/mysql_example.py
@@ -14,21 +14,18 @@ config = {
 DB = "state_db"
 TABLE_NAME = "history"
 STATE_COLUMN = "state"
-ACTION_COLUMN = "action"
-SCORE_COLUMN = "score"
+
 ID_COLUMN = "id"
 SHOW_DATABASE = "SHOW DATABASES"
 SHOW_TABLES = "SHOW TABLES"
 SHOW_DATA = "SELECT * FROM {}".format(TABLE_NAME)
-SHOW_GAMEID = "SELECT id FROM {}".format(TABLE_NAME)
-INSERT_DATA = "INSERT INTO {} ({}, {}, {}, {}) VALUES (%s, %s, %s, %s)".format(TABLE_NAME,
-                                                                               ID_COLUMN,
-                                                                               STATE_COLUMN,
-                                                                               ACTION_COLUMN,
-                                                                               SCORE_COLUMN)
+SHOW_GAME_ID = "SELECT id FROM {}".format(TABLE_NAME)
+
+INSERT_DATA = "INSERT INTO {} ({}, {}) VALUES (%s, %s)".format(TABLE_NAME, ID_COLUMN, STATE_COLUMN)
+
 SET_INITIAL = "ALTER TABLE {} AUTO_INCREMENT=1".format(TABLE_NAME)
 CREATE_TABLE = "CREATE TABLE {}" \
-               "(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state JSON, action JSON, score JSON)".format(TABLE_NAME)
+               "(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state JSON)".format(TABLE_NAME)
 CREATE_DB = "CREATE DATABASE {}".format(DB)
 DROP_DB = "DROP DATABASE {}".format(DB)
 DROP_TABLE = "DROP TABLE {}".format(TABLE_NAME)
@@ -56,7 +53,7 @@ def connect_to_database():
             print(err)
 
 
-def query_database(query, game_id, state, action, score):
+def query_database(query, game_id, state):
     db = Database()
     my_cursor = db.get_cursor()
     try:
@@ -64,8 +61,25 @@ def query_database(query, game_id, state, action, score):
             my_cursor.execute(query)
             show_results(my_cursor)
         else:
-            values = (game_id, state, action, score)
+            values = (game_id, state)
             my_cursor.execute(query, values)
+    except mysql.connector.Error as err:
+        print("Something might be wrong: {}".format(err))
+    finally:
+        db.cnx.commit()
+        my_cursor.close()
+        db.cnx.close()
+
+
+def get_bulk_data(query):
+    db = Database()
+    my_cursor = db.get_cursor()
+    try:
+        my_cursor.execute(query)
+        data_results = list()
+        for row in my_cursor.fetchall():
+            data_results.append(row)
+        return data_results
     except mysql.connector.Error as err:
         print("Something might be wrong: {}".format(err))
     finally:
@@ -79,30 +93,16 @@ def show_results(cursor):
         print(row)
 
 
-def test_db():
-    query_database(CREATE_DB, None, None, None, None)
-    query_database(SHOW_DATABASE, None, None, None, None)
-    query_database(CREATE_TABLE, None, None, None, None)
-    query_database(SHOW_TABLES, None, None, None, None)
-    state = [1, 1, 4, 1, 3, 2, 2, 1, 3, 2, 4, 1, 2, 4, 2, 4, 2, 1, 1, 4, 1, 4, 1, 4, 2, 1, 3, 2, 3, 3, 4, 3, 1, 1, 4, 2,
-             3,
-             2, 2, 3, 4, 3, 2, 3, 3, 4, 1, 2, 4, 4, 3, 3]
-    agent_action = 12
-    score = [0, 0, 0, 0]
-    query_database(INSERT_DATA, None, json.dumps(state), json.dumps(agent_action), json.dumps(score))
-    query_database(SHOW_DATA, None, None, None, None)
-
-
 def initialize_db():
-    query_database(CREATE_DB, None, None, None, None)
-    query_database(CREATE_TABLE, None, None, None, None)
+    query_database(CREATE_DB, None, None)
+    query_database(CREATE_TABLE, None, None)
 
 
 # initialize_db()
-# query_database(DROP_TABLE, None, None, None, None)
+# query_database(DROP_DB, None, None)
 
 # test_db()
 # query = "show variables like 'max_allowed_packet'"
 # query = "show global status like 'Max_used_connections
 
-query_database(SHOW_DATA, "TABLE_NAME", None, None, None)
+query_database(SHOW_DATA, "TABLE_NAME", None)
