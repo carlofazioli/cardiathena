61466a719e5e90eba42e258527be650eddc070c0
davidjha <davidjha21@gmail.com>
2020-02-06 13:06:21 -0500

    Removed initialize_db() here, because it doesn't need to be called multiple times each time play_hearts.py is ran.

--- play_hearts.py
+++ play_hearts.py
@@ -1,7 +1,6 @@
 from base import GameManager
 from adjudicator.hearts_adjudicator import HeartsAdjudicator
 from agent.RandomHeartsAgent import RandomHeartsAgent
-from database import mysql_example as mysql
 
 # Create the players, the adjudicator, and the game object.
 agent_1 = RandomHeartsAgent()
@@ -12,9 +11,6 @@ adj = HeartsAdjudicator()
 game = GameManager(agent_list=[0, agent_1, agent_2, agent_3, agent_4],
                    adjudicator=adj)
 
-mysql.initialize_db()
-
 # Play a game.
 game.play_game()
 game.save_game()
-
