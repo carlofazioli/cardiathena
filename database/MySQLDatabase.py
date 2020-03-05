0b88dbe8a3a13f691aea8a795180507c2614fc3a
davidjha <davidjha21@gmail.com>
2020-03-05 09:31:37 -0500

    Database/table initialization functions removed. Database/tables are pre-configured in a db file.

--- database/MySQLDatabase.py
+++ database/MySQLDatabase.py
@@ -1,20 +1,9 @@
 import mysql.connector
 import json
 from mysql.connector import errorcode
-from database.MySQLVariables import CREATE_DB, CREATE_GAME_TABLE, CREATE_AGENT_TABLE, INSERT_PLAYERS, HEARTS_AGENTS, \
-    INSERT_AGENT
-from database.MySQLVariables import STATE_TABLE
-from database.MySQLVariables import ID_COLUMN
-from database.MySQLVariables import SELECT_ALL_FROM_PLAYER_TABLE
-from database.MySQLVariables import SELECT_ALL_FROM_STATE_TABLE
-from database.MySQLVariables import DROP_TABLE
-from database.MySQLVariables import DROP_DB
+from database.MySQLVariables import STATE_TABLE, GAME_ID_COLUMN
 from database.MySQLVariables import SELECT_GAME_ID
-from database.MySQLVariables import SHOW_TABLES
-from database.MySQLVariables import SHOW_DATABASE
-from database.MySQLVariables import CONFIG_INITIALIZE
-from database.MySQLVariables import CONFIG2
-
+from database.MySQLVariables import CONFIG
 
 
 class MySQLDatabase:
@@ -22,8 +11,6 @@ class MySQLDatabase:
     Database connection object
     """
     def __init__(self):
-        if get_connection() is None:
-            initialize_db()
         self.cnx = get_connection()
 
     def get_cursor(self):
@@ -37,7 +24,7 @@ def get_connection():
     :returns cnx: MySQL connection object
     """
     try:
-        cnx = mysql.connector.connect(**CONFIG2)
+        cnx = mysql.connector.connect(**CONFIG)
         return cnx
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -48,63 +35,7 @@ def get_connection():
             print(err)
 
 
-def initialize_db():
-    """
-    Initializes the database. Creates a new game state database and game state table.
-    """
-    try:
-        cnx = mysql.connector.connect(**CONFIG_INITIALIZE)
-        my_cursor = cnx.cursor()
-        my_cursor.execute(CREATE_DB)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        else:
-            print(err)
-    finally:
-        cnx.commit()
-        my_cursor.close()
-        cnx.close()
-
-
-def initialize_table():
-    query_database(CREATE_AGENT_TABLE)
-
-    for agent, attributes in HEARTS_AGENTS.items():
-        name = attributes["name"]
-        version = attributes["version"]
-        insert_players(INSERT_AGENT, name, version)
-    #query_database(CREATE_GAME_TABLE)
-    #query_database(CREATE_STATE_TABLE)
-
-
-def insert_players(query, name, version):
-    """
-    Establishes a connection to the database, and executes an insert query. Inserts state data into MySQL database
-
-    :param query: predefined insert query in MySQLVariables.py
-    :param game_uuid: is the game uuid passed in as a hex and saved as binary
-    :param players: is the players that participated in a game
-    """
-    db = MySQLDatabase()
-    my_cursor = db.get_cursor()
-    try:
-        values = (None, name, version)
-        my_cursor.execute(query, values)
-        return True
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_NO_SUCH_TABLE:
-            return False
-        else:
-            print(err)
-        print("Something might be wrong: {}".format(err))
-    finally:
-        db.cnx.commit()
-        my_cursor.close()
-        db.cnx.close()
-
-
-def query_database(query):
+def query_database(query, values):
     """
     Used to execute predefined basic queries, in MySQLVariables.py, without any values. Prints the results to console.
     IE 'SHOW_DATABASE' will show all the databases on the server.
@@ -112,22 +43,13 @@ def query_database(query):
     db = MySQLDatabase()
     my_cursor = db.get_cursor()
     try:
-        my_cursor.execute(query)
-        show_results(my_cursor)
+        my_cursor.execute(query, values)
     except mysql.connector.Error as err:
         print("Something might be wrong: {}".format(err))
     finally:
         db.cnx.commit()
         my_cursor.close()
         db.cnx.close()
-
-
-def show_results(cursor):
-    """
-    Helper method that prints the results of a query.
-    """
-    for row in cursor.fetchall():
-        print(row)
 
 
 def insert_state(file):
@@ -138,7 +60,7 @@ def insert_state(file):
                 "ENCLOSED BY '\"' " \
                 "LINES TERMINATED BY '\n'" \
                 "IGNORE 1 LINES" \
-                "(state, action, score)".format(file, STATE_TABLE)
+                "(deck, action, score, game_uuid)".format(file, STATE_TABLE)
         my_cursor.execute(query)
     except mysql.connector.Error as err:
         print(err)
@@ -159,7 +81,7 @@ def get_data_by_key(id_key):
     my_cursor = db.get_cursor()
 
     try:
-        query= "SELECT {} FROM {} WHERE {}={}".format("*", STATE_TABLE, ID_COLUMN, id_key)
+        query= "SELECT {} FROM {} WHERE {}={}".format("*", STATE_TABLE, GAME_ID_COLUMN, id_key)
         my_cursor.execute(query)
         data = my_cursor.fetchall()
         return data
@@ -231,11 +153,6 @@ def test_get_data_example():
     print(score)
 
 
-#query_database(DROP_DB)
-#query_database(CREATE_PLAYERS_TABLE)
-#query_database(CREATE_STATE_TABLE)
-#query_database(SHOW_DATABASE)
-#query_database(SHOW_TABLES)
 #query_database(SELECT_ALL_FROM_PLAYER_TABLE)
 #query_database(SELECT_ALL_FROM_STATE_TABLE)
 #test_get_data_example()
