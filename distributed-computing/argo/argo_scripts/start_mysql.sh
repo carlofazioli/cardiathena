3e3b268f877a4f79462bc1f60bac106e6bb67f3e
davidjha <davidjha21@gmail.com>
2020-03-12 17:14:17 -0400

    Removed stop container. Container never exits gracefully so stop doesn't do anything. Renamed file.

--- distributed-computing/argo/argo_scripts/start_mysql.sh
+++ distributed-computing/argo/argo_scripts/start_mysql.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-singularity instance stop mysqlheart
 singularity instance start -e -c --bind ${SCRATCH}/mysql/var/lib/mysql/:/var/lib/mysql --bind ${SCRATCH}/mysql/run/mysqld:/run/mysqld mysql_latest.sif mysqlheart
 singularity run instance://mysqlheart
