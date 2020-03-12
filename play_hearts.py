3237154a721dc7baeea9d01221f792c1e589e2dc
aphelps4 <alp7@verizon.net>
2020-03-12 16:40:41 -0400

    Created basic handling of leading and following for Shooter.py

--- play_hearts.py
+++ play_hearts.py
@@ -3,13 +3,14 @@ from base import GameManager
 from adjudicator.hearts_adjudicator import HeartsAdjudicator
 from adjudicator.state import HeartsState
 from agent.RandomHeartsAgent import RandomHeartsAgent
+from agent.Shooter import Shooter
 
 
 # Create the players, the adjudicator, and the game object.
 agent_1 = RandomHeartsAgent()
 agent_2 = RandomHeartsAgent()
 agent_3 = LowLayer()
-agent_4 = RandomHeartsAgent()
+agent_4 = Shooter()
 adj = HeartsAdjudicator()
 state = HeartsState()
 game = GameManager(agent_list=[0, agent_1, agent_2, agent_3, agent_4],
