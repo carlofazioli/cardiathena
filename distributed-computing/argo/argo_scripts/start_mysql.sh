786aaaf476efb1f3e80c4c1517be3cbaf6e574b7
davidjha <davidjha21@gmail.com>
2020-03-14 16:24:41 -0400

    Update start_mysql.sh

--- distributed-computing/argo/argo_scripts/start_mysql.sh
+++ distributed-computing/argo/argo_scripts/start_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Write the host of the MySQL server container to a file.
-hostname > mysql_hostname
+hostname > $SCRATCH/mysql_hostname
 
 # Start instance.
 singularity instance start -e -c --bind ${SCRATCH}/mysql/var/lib/mysql/:/var/lib/mysql --bind ${SCRATCH}/mysql/run/mysqld:/run/mysqld mysql_latest.sif mysqlheart
