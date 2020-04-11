4f166a8a3917f0d107017a3857d85e07984d90e9
davidjha <davidjha21@gmail.com>
2020-04-10 21:29:33 -0400

    Added for batch uploading to database.

--- database/mysql/hearts/batchUpload.py
+++ database/mysql/hearts/batchUpload.py
@@ -1,4 +1,53 @@
+import os
+import glob
+import shutil
 import csv
-from database.mysql.hearts import HeartsMySQLDatabase as db
-from database.mysql.hearts.HeartsMySQLVariables import INSERT_GAME, CSV_DIR, MYSQL_SERVER
+import pandas as pd
+import mysql.connector
+from database.mysql.hearts.HeartsMySQLDatabase import MySQLDatabase
+from database.mysql.hearts.HeartsMySQLVariables import CSV_DIR, STATE_TABLE
 
+game_table_files = glob.glob(os.path.join(CSV_DIR, "*_gametable.csv"))
+state_table_files = glob.glob(os.path.join(CSV_DIR, "*statetable.csv"))
+
+
+def insert_game_table():
+    dbs = MySQLDatabase()
+    my_cursor = dbs.get_cursor()
+    try:
+        for file in game_table_files:
+            query = "LOAD DATA LOCAL INFILE '{}' INTO TABLE {} FIELDS TERMINATED BY ',' " \
+                "ENCLOSED BY '\"' " \
+                "LINES TERMINATED BY '\n'" \
+                "IGNORE 1 LINES" \
+                "(time, agent1, agent2, agent3, agent4, game_uuid)".format(file, STATE_TABLE)
+            my_cursor.execute(query)
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        dbs.cnx.commit()
+        my_cursor.close()
+        dbs.cnx.close()
+
+
+def insert_state_table():
+    dbs = MySQLDatabase()
+    my_cursor = dbs.get_cursor()
+    try:
+        for file in state_table_files:
+            query = "LOAD DATA LOCAL INFILE '{}' INTO TABLE {} FIELDS TERMINATED BY ',' " \
+                "ENCLOSED BY '\"' " \
+                "LINES TERMINATED BY '\n'" \
+                "IGNORE 1 LINES" \
+                "(deck, action, score, game_uuid)".format(file, STATE_TABLE)
+            my_cursor.execute(query)
+    except mysql.connector.Error as err:
+        print(err)
+    finally:
+        dbs.cnx.commit()
+        my_cursor.close()
+        dbs.cnx.close()
+
+
+insert_game_table()
+insert_state_table()
