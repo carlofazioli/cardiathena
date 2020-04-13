1062a522b855a9f68fe96824dc4e2ba43d6c5ff7
davidjha <davidjha21@gmail.com>
2020-04-12 20:48:13 -0400

    Update clean_argo_logs.sh

--- distributed-computing/argo/helper_scripts/clean_argo_logs.sh
+++ distributed-computing/argo/helper_scripts/clean_argo_logs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -f ${HOME}/rsync ]
-then
-  cp /bin/rsync ${HOME}/rsync
 
 $HOME/rsync -a --delete $SCRATCH/empty_dir/ $SCRATCH/logs_argo/out/
 $HOME/rsync -a --delete $SCRATCH/empty_dir/ $SCRATCH/logs_argo/err/
