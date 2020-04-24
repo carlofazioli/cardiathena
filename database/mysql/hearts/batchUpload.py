0998ecfd923b2bd46f9dff6ebb0f0010daf5f670
davidjha <davidjha21@gmail.com>
2020-04-24 11:57:28 -0400

    Fixed upload to database. Commits every 10000 data files.

--- database/mysql/hearts/batchUpload.py
+++ database/mysql/hearts/batchUpload.py
@@ -4,23 +4,14 @@ import shutil
 import mysql
 from mysql.connector import errorcode
 
-#from database.mysql.hearts.HeartsMySQLVariables import CSV_DIR, CONFIG, GAME_TABLE, ARCHIVE_DIR, STATE_TABLE
-from HeartsMySQLVariables import CSV_DIR
-from HeartsMySQLVariables import ARCHIVE_DIR
-from HeartsMySQLVariables import STATE_TABLE
-from HeartsMySQLVariables import GAME_TABLE
-from HeartsMySQLVariables import CONFIG
-from HeartsMySQLVariables import SCRATCH_DIR
+from database.mysql.hearts.HeartsMySQLVariables import CSV_DIR, CONFIG, GAME_TABLE, ARCHIVE_DIR, STATE_TABLE
+#from HeartsMySQLVariables import CSV_DIR
+#from HeartsMySQLVariables import ARCHIVE_DIR
+#from HeartsMySQLVariables import STATE_TABLE
+#from HeartsMySQLVariables import GAME_TABLE
+#from HeartsMySQLVariables import CONFIG
+#from HeartsMySQLVariables import SCRATCH_DIR
 
-print(CSV_DIR)
-
-game_table_files = glob.glob(os.path.join(CSV_DIR, "*_gametable.csv"))
-state_table_files = glob.glob(os.path.join(CSV_DIR, "*_statetable.csv"))
-
-
-with open(SCRATCH_DIR + "/{}".format("sql_logs"), 'w') as fil:
-    fil.write("csv paths have been loaded\n")
-print("csv paths have been loaded")
 
 class MySQLDatabase:
     """
@@ -51,63 +42,88 @@ def get_connection():
             print(err)
 
 
+def move_files(files_list):
+    for entry in files_list:
+        shutil.move(entry, ARCHIVE_DIR)
+    return list()
+
+
 def insert_game_table():
+    game_table_files = glob.glob(os.path.join(CSV_DIR, "*_gametable.csv"))
+    game_table_files.sort()
     dbs = MySQLDatabase()
     my_cursor = dbs.get_cursor()
     saved_files = list()
+    count = 10000
+
     try:
         for i, file in enumerate(game_table_files):
-            query = "LOAD DATA LOCAL INFILE '{}' INTO TABLE {} FIELDS TERMINATED BY ',' " \
+            query = "LOAD DATA LOCAL INFILE '{}' IGNORE INTO TABLE {} FIELDS TERMINATED BY ',' " \
                 "LINES TERMINATED BY '\n'" \
                 "(time, agent1, agent2, agent3, agent4, game_uuid)".format(file, GAME_TABLE)
             my_cursor.execute(query)
             saved_files.append(file)
-            # with open(SCRATCH_DIR + "/{}".format("sql_logs"), 'a') as fil:
-            #    fil.write(file + " has been moved\n")
-            if i == 10000:
-                break
-
+            if i == count:
+                dbs.cnx.commit()
+                my_cursor.close()
+                my_cursor = dbs.get_cursor()
+                saved_files = move_files(saved_files)
+                count = count + 10000
     except mysql.connector.Error as err:
-        print(err)
+        # Check for foreign key constraint failure
+        if err.errno == 1452:
+            print(err)
+            pass
+        # Check for duplicate game_uuid
+        elif err.errno == 1062:
+            pass
+        else:
+            print(err)
     finally:
         dbs.cnx.commit()
         my_cursor.close()
         dbs.cnx.close()
-        for file in saved_files:
-            shutil.move(file, ARCHIVE_DIR)
+        saved_files = move_files(saved_files)
 
 
 def insert_state_table():
+    state_table_files = glob.glob(os.path.join(CSV_DIR, "*_statetable.csv"))
+    state_table_files.sort()
     dbs = MySQLDatabase()
     my_cursor = dbs.get_cursor()
     saved_files = list()
+    count = 10000
     try:
         for i, file in enumerate(state_table_files):
-            query = "LOAD DATA LOCAL INFILE '{}' INTO TABLE {} FIELDS TERMINATED BY ',' " \
+            query = "LOAD DATA LOCAL INFILE '{}' IGNORE INTO TABLE {} FIELDS TERMINATED BY ',' " \
                 "ENCLOSED BY '\"' " \
                 "LINES TERMINATED BY '\n'" \
                 "(deck, action, score, game_uuid)".format(file, STATE_TABLE)
             my_cursor.execute(query)
             saved_files.append(file)
-            # with open(SCRATCH_DIR + "/{}".format("sql_logs"), 'a') as fil:
-            #    fil.write(file + " has been moved\n")
-            if i == 10000:
-                break
-
+            if i == count:
+                dbs.cnx.commit()
+                my_cursor.close()
+                my_cursor = dbs.get_cursor()
+                saved_files = move_files(saved_files)
+                count = count + 10000
     except mysql.connector.Error as err:
-        print(err)
+        # Check for foreign key constraint failure
+        if err.errno == 1452:
+            print(err)
+            pass
+        # Check for duplicate game_uuid
+        elif err.errno == 1062:
+            print(err)
+            pass
+        else:
+            print(err)
     finally:
         dbs.cnx.commit()
         my_cursor.close()
         dbs.cnx.close()
-        for file in saved_files:
-            shutil.move(file, ARCHIVE_DIR)
+        saved_files = move_files(saved_files)
 
 
-
-while game_table_files:
-    insert_game_table()
-
-
-while state_table_files:
-    insert_state_table()
+insert_game_table()
+insert_state_table()
